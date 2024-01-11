c17e2a30565a3ff523aa8d7990fb80587c37cc50
DevWambugu <venusdata100@gmail.com>
2024-01-11 22:06:24 +0300

    second trial

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,17 +2,22 @@
 
 '''This function determines
 if all boxes can be opended'''
-def canUnlockAll(boxes)
+def canUnlockAll(boxes):
     '''Determines if all boxes can be opened'''
     listLength = len(boxes)
-    for box in boxes:
-        boxLength = len(box)
+    for box in range (len(boxes)):
+        boxLength = len(boxes[box])
+        box_opened = True
         for count in range (boxLength):
-            key = box[count]
-            for i in (range(boxes) - 1):
-              if key == boxes[i + 1]:
-                 n == 1
-              else:
-                 n == 0
-                 return False
-            return True
+            key = boxes[box][count]
+            key_found = False
+            for i in range(listLength - 1):
+              if key in boxes[i + 1]:
+                 key_found = True
+                 break
+            if not key_found:
+              box_opened = False
+              break
+        if not box_opened:
+            return False
+    return True
